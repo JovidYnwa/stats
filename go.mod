83e6096b272502254a7a842577fdbbf4102ccfaf
Jovid Nurov <jovid.9@gmail.com>
2020-09-25 17:46:34 +0500

    Initializing stats

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JovidYnwa/stats
+
+go 1.15
+
+require github.com/JovidYnwa/bank v0.0.0-20200925123634-618229cd6b84
---
+++ pkg/stats/stats.go
@@ -0,0 +1,38 @@
+package stats
+
+import (
+	"github.com/JovidYnwa/bank/pkg/types"
+)
+
+//Avg рассчитать среднюю сумму платежа.
+func Avg(payments []types.Payment) types.Money {
+	paySum := types.Money(0)
+	payCount := types.Money(0)
+	//pyaments := []types.Payment{}
+	if len(payments) == 1 {
+		return payments[0].Amount
+	}
+
+	for i := 0; i < len(payments); i++ {
+		paySum += payments[i].Amount
+		payCount++
+	}
+
+	return paySum / payCount
+}
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sum := 0
+
+	if len(payments) == 1 {
+		return payments[0].Amount
+	}
+
+	for _, payment := range payments {
+		if category == payment.Category {
+			sum = sum + int(payment.Amount)
+		}
+	}
+
+	return types.Money(sum)
+}
